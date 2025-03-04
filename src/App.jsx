714e66fe694386338934620e04a2283f4a54ad8a
Madhav Arora <madhavarora132005@gmail.com>
2025-03-04 22:54:32 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='text-center flex justify-center items-center h-screen bg-red-300 text-9xl text-slate-900 font-bold'>GenX</div>
+    <div className='text-center flex justify-center items-center h-screen bg-red-300 text-9xl text-slate-900 font-bold'>GenX
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur autem ratione iste doloribus unde neque. Esse in eius ad. Natus.</p>
+    </div>
   )
 }
 
