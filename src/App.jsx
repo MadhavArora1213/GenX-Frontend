8f8b258b6471069f3a8f4125a72acb3a3a5072e9
Madhav Arora <madhavarora132005@gmail.com>
2025-03-04 22:42:22 +0530

    Make React project and Setup tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-center flex justify-center items-center h-screen bg-red-300 text-9xl text-slate-900 font-bold'>GenX</div>
+  )
+}
+
+export default App
