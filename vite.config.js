6c50a2514154924118042b0fd217e7322dda9061
Madhav Arora <madhavarora132005@gmail.com>
2025-03-14 16:31:05 +0530

    update meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import sitemap from "vite-plugin-pages-sitemap";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://yourwebsite.com",
+      dynamicRoutes: ["/editor"], // Add dynamic routes if needed
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
